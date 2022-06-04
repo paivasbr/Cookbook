a60cd9e9fe0c3f594d282da2e5d5329921e29af6
Brenda Paiva <contatobrendapaiva@outlook.com>
2022-06-04 03:01:26 -0300

    img

--- cookbook-rogers/src/components/Popular.jsx
+++ cookbook-rogers/src/components/Popular.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 function Popular() {
-
-    
-
+    const [popular, setPopular] = useState([]);
 
 
     useEffect(() => {
@@ -13,14 +12,48 @@ function Popular() {
     const getPopular = async () => {
         const api = await fetch(
             `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-            );
+        );
         const data = await api.json();
-        console.log(data);
-
+        setPopular(data.recipes);
+        console.log(data.recipes);
     };
 
 
-  return <div>Popular</div>;
+    return (
+        <div>
+            {popular.map((recipe) => {
+                return (
+                    <Wrapper>
+                        <h3>Popular Picks</h3>
+                        {popular.map((recipe) => {
+                            return (
+                                <Card>
+                                    <p>{recipe.title}</p>
+                                    <img src={recipe.image} alt={recipe.title} />
+                                </Card>
+                            );
+                        })}
+                    </Wrapper>
+                );
+            })}
+        </div>
+    );
 }
+
+const Wrapper = styled.div`
+    margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+    min-height: 25rem;
+    border-radius: 2rem;
+    overflow: hidden;
+
+    img{
+        border-radius: 2rem;
+    }
+
+`;
+
 
 export default Popular;

7371697bc0c4c85df63f6c137a0e7e64b383f311
Brenda Paiva <contatobrendapaiva@outlook.com>
2022-06-05 17:29:52 -0300

    Recipes

---
+++ cookbook-rogers/src/pages/Recipe.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import styles from 'styled-components';
+import { useParams } from 'react-router-dom';
+
+function Recipe() {
+  return <div>Recipe</div>
+}
+
+export default Recipe;

a105a1a0b4b09ee8164f0f693f6a910057d98695
Brenda Paiva <contatobrendapaiva@outlook.com>
2022-06-05 00:12:42 -0300

    Category

--- cookbook-rogers/src/App.js
+++ cookbook-rogers/src/App.js
@@ -1,10 +1,11 @@
 import Pages from './pages/Pages';
+import Category from "./components/Category"
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World!</h1>
-     <Pages />
+      <Category />
+      <Pages />
     </div>
   );
 }
